6474c825432aec1e272f3228b780b47c132be33b
fcl-93 <fabiocl93@gmail.com>
2017-04-07 17:11:08 +0100

    Network Scanner - Send Initial Config

--- app.js
+++ app.js
@@ -14,23 +14,28 @@ var activePlugs = [];
 networkScanner(); // Starts a network monitor
 
 
-
 wss.on('connection', function connection(ws) {
-
+    console.log(ws);
     ws.on('message', function (message) {
-        if(message === "initconfig") {
-            initConfig();
-        }else if(message === "heartbeat"){
+        if(message === "heartbeat"){
             heatBeat(position);
+        }
+        if(message === "position"){
+            array.forEach(function(result, index) {
+                if(result[property] === value) {
+                    //Remove from array
+                    array.splice(index, 1);
+                }
+            });
         }
     });
 });
 
 
 function initConfig(){
-    var position  = activePlugs.length();
+    var position  = activePlugs.length;
     var orientation;
-    if(activePlugs.length() >= 11) {
+    if(activePlugs.length >= 11) {
         orientation = 0;
     }else{
         orientation = 1
@@ -38,54 +43,73 @@ function initConfig(){
     var delay = 100;
     var relayState = 0;
     var personNear = 1;
-    ws.send({'orientation': orientation,'position': position, 'delay':delay,'relayState': relayState,'personNear':personNear});
+    return {'orientation': orientation,'position': position, 'delay':delay,'relayState': relayState,'personNear':personNear};
 }
 
-function heatBeat(position){
-}
+
+
+
 
 function networkScanner(){
     console.log("Starting Monitoring Service");
+
     var sequence = [
         mdns.rst.DNSServiceResolve()
         , mdns.rst.getaddrinfo({families: [4] })
     ];
-    try {
-    var browser = mdns.createBrowser(mdns.tcp('http'),{resolverSequence: sequence});
+
+    try{
+        var browser = mdns.createBrowser(mdns.tcp('http'),{resolverSequence: sequence});
+    } catch (ex) {
+        console.log('Something went wrong creating the webSocket' + ex)
+    }
 
     browser.on('serviceUp', function(service) {
         if(service.host.substring(0, 4) === "plug") {
             console.log("A new plug is on: ", service.host.substring(0, service.host.length - 1) + "");
-            activePlugs.push(service.host);
+            var plugObject = {name:service.host.substring(0, service.host.length - 1), 'initStateSet': 0 , 'initTime': Date.now()};
+            var initConfigs = initConfig();
+            try {
+                var plugSocket = new WebSocket('ws://'+plugObject['name']);
+                plugSocket.send(initConfigs);      //Send startUp Data
+                plugObject = plugObject.concat(initConfigs);
+                plugObject['initStateSet'] = 1;     //Plug has got it's startup Data
+                plugObject['initTime'] = Date.now();
+                console.log(plugObject['initTime']);
+            }
+            catch (ex){
+                console.log(ex);
+            }
+            activePlugs.push(plugObject);
         }else{
-            console.log("Ignoring Device" + service.host);
         }
     });
+
+    browser.on('error', function(error) {
+        //console.log('An error occured: ' + error);
+    });
+
     browser.on('serviceDown', function(service) {
         if(service.name.substring(0,4) === "plug") {
             console.log("", service.name + ".local" + " is now disconnected.");
-            activePlugs.pop(service.name);
-        }else{
-            console.log("Ignoring Device" + service.name);
+            findAndRemove(activePlugs,'name',service.name + ".local");
+            //console.log("There are " + activePlugs.length  + " active plugs")
+        }else {
+            //console.log("Ignoring Device" + service.name);
         }
     });
 
-        browser.start();
-    }
-    catch (e) {
-        console.log("An error was detected" + e);
-    }
+    browser.start();
 }
 
 
-function changeRelayState(){
-    return relayState;
+function findAndRemove(array, property, value) {
+    array.forEach(function(result, index) {
+        if(result[property] === value) {
+            //Remove from array
+            array.splice(index, 1);
+
+        }
+    });
 }
 
-function getPlugCurrentState(){
-
-}
-
-function personNearCheck(){
-    return personNear;
-}

8f3a09f76c98951344f76c0184658272621a44b2
fcl-93 <fabiocl93@gmail.com>
2017-04-07 20:19:39 +0100

    Get Config Through Rest

--- app.js
+++ app.js
@@ -1,38 +1,58 @@
 var express = require('express');
-
+var WebSocket = require('ws');
+var mdns = require('mdns');
+var bodyParser = require('body-parser');
+var io = require('socket.io-client');
 //var path = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+//
 
-var WebSocket = require('ws');
-var mdns = require('mdns');
+var app = express();
 var wss = new WebSocket.Server({ port: 8080 });
+
+var port = process.env.PORT || 7777; //API Rest Port
+var router = express.Router();
+
+
 var activePlugs = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//app.use('/plug', router); //Adds Prefix on every api url
+
+app.listen(port);
+console.log("Server is listening on port: " +port);
 
 networkScanner(); // Starts a network monitor
 
+//velocidade, posição inicial
+app.get('/plug/:plugid',function(req,res){
+    var plugId = req.params.plugid;
+    var plugName = 'plug'+plugId+'.local';
+    if(activePlugs.length > 0) {
+        for(var i = 0; i < activePlugs.length; i++){
+            if (activePlugs[i].name === plugName) {
+                var velocity = activePlugs[i].delay
+                var initTime = activePlugs[i].initTime;
+                var orientation = activePlugs[i].orientation;
+                break; // don't need to find any more plugs
+            }
+        }
+        res.json({'position':((Date.now() - initTime)%(velocity*12))/100, 'velocity': velocity, 'orientation': orientation});
+    }else{
+        res.json("No plugs are active.");
+    }
+});
 
-wss.on('connection', function connection(ws) {
-    console.log(ws);
-    ws.on('message', function (message) {
-        if(message === "heartbeat"){
-            heatBeat(position);
-        }
-        if(message === "position"){
-            array.forEach(function(result, index) {
-                if(result[property] === value) {
-                    //Remove from array
-                    array.splice(index, 1);
-                }
-            });
-        }
-    });
+app.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 
 function initConfig(){
+    /*  Initial Config Manager  */
     var position  = activePlugs.length;
     var orientation;
     if(activePlugs.length >= 11) {
@@ -51,7 +71,8 @@ function initConfig(){
 
 
 function networkScanner(){
-    console.log("Starting Monitoring Service");
+    /*  Network Scanner and gives socket it's initial configs   */
+   console.log("Starting Monitoring Service");
 
     var sequence = [
         mdns.rst.DNSServiceResolve()
@@ -66,21 +87,27 @@ function networkScanner(){
 
     browser.on('serviceUp', function(service) {
         if(service.host.substring(0, 4) === "plug") {
+            //console.log(service);
             console.log("A new plug is on: ", service.host.substring(0, service.host.length - 1) + "");
             var plugObject = {name:service.host.substring(0, service.host.length - 1), 'initStateSet': 0 , 'initTime': Date.now()};
             var initConfigs = initConfig();
             try {
-                var plugSocket = new WebSocket('ws://'+plugObject['name']);
-                plugSocket.send(initConfigs);      //Send startUp Data
-                plugObject = plugObject.concat(initConfigs);
-                plugObject['initStateSet'] = 1;     //Plug has got it's startup Data
-                plugObject['initTime'] = Date.now();
-                console.log(plugObject['initTime']);
+                //Creates a new socket
+                var plugSocket = io('http://'+plugObject['name']+':5000');
+
+                plugSocket.on('connect',function(){
+                    plugSocket.emit('initConfig',initConfigs);
+                    Object.assign(plugObject, plugObject, initConfigs);
+                    console.log(plugObject);
+                    plugObject['initStateSet'] = 1;     //Plug has got it's startup Data
+                    plugObject['initTime'] = Date.now();
+                    activePlugs.push(plugObject);
+                });      //Send startUp Data
             }
             catch (ex){
-                console.log(ex);
+                console.log("Socket is wrong");
             }
-            activePlugs.push(plugObject);
+
         }else{
         }
     });
@@ -106,9 +133,7 @@ function networkScanner(){
 function findAndRemove(array, property, value) {
     array.forEach(function(result, index) {
         if(result[property] === value) {
-            //Remove from array
             array.splice(index, 1);
-
         }
     });
 }

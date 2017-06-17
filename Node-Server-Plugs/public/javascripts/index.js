1bc5f08f81655d488c344ab43878a06387f49e67
fcl-93 <fabiocl93@gmail.com>
2017-06-17 14:08:05 +0100

    New GUI

--- Node-Server-Plugs/public/javascripts/index.js
+++ Node-Server-Plugs/public/javascripts/index.js
@@ -1,83 +1,82 @@
 /**
  * Created by vmcb on 02-06-2017.
  */
-var newLeds = "" +
-    "<div>" +
-    "<label>LED:" +
-    "<input name='position' class='position'>" +
-    "<label>Orientation:</label>" +
-    "<input name='orientation' class='orientation'>" +
-    "<label>Red:</label>" +
-    "<input name='red' class='red'>" +
-    "<label>Green:</label>" +
-    "<input name='green' class='green'>" +
-    "<label>Blue:</label>" +
-    "<input name='blue' class='blue'>" +
-    "<button class='removeLED'>Remove LED</button>" +
-    "<br>" +
+var newLedRow = "" +
+    "<div class='row'>" +
+        "<div class='col-xs-3'>"+
+        "<input name='position' class='form-control position'>" +
+        "</div>"+
+        "<div class='col-xs-3'>"+
+            "<input name='orientation' class='form-control orientation'>" +
+        "</div>"+
+        "<div class='col-xs-3'>"+
+            "<div class='input-group colorpicker-component'>" +
+            "<input type='text' value='#00AABB' class='form-control' />" +
+            "<span class='input-group-addon'><i></i></span>" +
+            "</div>" +
+        "</div>"+
+        "<div class='col-xs-3'>"+
+            "<i class='glyphicon glyphicon-trash'></i>" +
+        "</div>"+
     "</div>";
 
+
+//Adds a new Box;
+
 function addNewPlugForm(plugName) {
-    return "" +
-        "<div>" +
-            "<p>Device name:"+ plugName +"</p>" +
-            "<div>" +
-                "<label>LED:</label>" +
-                "<input name='position' class='position'>" +
-                "<label>Orientation:</label>" +
-                "<input name='orientation' class='orientation'>" +
-                "<label>Red:</label>" +
-                "<input name='red' class='red'>" +
-                "<label>Green:</label>" +
-                "<input name='green' class='green'>" +
-                "<label>Blue:</label>" +
-                "<input name='blue' class='blue'>" +
-                "<button class='add'> Add more LEDs</button>" +
-                "<br>" +
-            "</div>" +
-            "<div>" +
-                "<label>Velocity:</label> " +
-                "<input name='velocity' class='velocity'>" +
-            "</div>" +
-            "<div>" +
-                "<button class='configure'>Configure Movements</button>" +
-                "<button class='stop'> Stop Movements</button>" +
-            "</div>" +
-        "</div>";
+    return
+
+
+
+
+
 }
+
 $().ready(function () {
     var socket = io(location.protocol + "//" + location.host);
     socket.on('new_plug', function (data) {
         $("#plugs").append(addNewPlugForm(data.name));
     });
 
-    $(".add").click( function (e) {
-        $(this).parent().prev().prev().append(newLeds);
+    //Add  a new Led
+    $(".glyphicon-plus").click( function (e) {
+        $(this).parent().parent().prev().after(newLedRow);
+        //Binds the colorpicker to children
+        $(this).parent().parent().prev().children().eq(2).children().colorpicker({
+                color: '#AA3399',
+                format: 'rgb'
+            });
     });
 
-    $(".configure").click(function (e) {
+    // Remove a Led
+    $(document).on("click", ".glyphicon-trash", function (e) {
+        $(this).parent().parent().remove();
+    });
+
+    //Sends Data To The Socket
+    $(document).on("click",".glyphicon-play",function (e) {
         var leds = [];
-        $(this).parent().parent().find(".position").each(function (index, elem) {
+        $(this).parent().parent().parent().find(".position").each(function (index, elem) {
             var position = $(elem);
-            var orientation = $(elem).next().next();
-            var red = orientation.next().next();
-            var green = red.next().next();
-            var blue = green.next().next();
-            leds.push({position:position.val(),orientation:orientation.val(),red:red.val(),green:green.val(),blue:blue.val()})
+            var orientation = $(elem).parent().next().children();
+            var color = $(this).parent().parent().children().eq(2).children().colorpicker("getValue");
+
+            color = color.replace("rgb(","");
+            color = color.replace(")","");
+            var red = color.split(",")[0];
+            var green = color.split(",")[1];
+            var blue = color.split(",")[2];
+            leds.push({position:position.val(),orientation:orientation.val(),red: red, green: green, blue:blue})
         });
-        var velocity = parseInt($(this).parent().prev().find(".velocity").val());
+        var velocity = parseInt($(this).parent().parent().children().eq(0).children().eq(0).children().eq(1).val());
         var data = {leds:leds,velocity:velocity};
         $.post('/plug/'+$(this).parent().prev().find(":hidden").val().match(/\d+/)[0],data);
     });
 
-    $(".stop").click(function (e) {
+    //Stop Socket Spinning
+    $(".glyphicon-stop").click(function (e) {
         $.post('/plug/'+$(this).parent().prev().find(":hidden").val().match(/\d+/)[0]+'/stopLeds');
     });
 
-    $(document).on("click", ".removeLED", function (e) {
-        console.log("click");
-        console.log($(this).parent());
-        $(this).parent().empty();
-    });
 
 });
